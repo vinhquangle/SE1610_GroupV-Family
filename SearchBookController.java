c654d5d179db61763938244a8ba38b10db3c6bba
vinhquangle <haclong0512@gmail.com>
2022-09-21 17:07:31 +0700

    SearchBookController
    Locate: packages controller.search

---
+++ SearchBookController.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.search;
+
+import dao.BookDAO;
+import dto.BookDTO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Quang Vinh-21/09/2022
+ * Controller dung de search book by ISBN/Title/Authors
+ */
+@WebServlet(name = "SearchBookController", urlPatterns = {"/SearchBookController"})
+public class SearchBookController extends HttpServlet {
+
+    private static final String ERROR = "error.jsp";
+    private static final String SUCCESS = "JSP/HomePage/homePage.jsp";
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        try {
+            request.setCharacterEncoding("UTF-8");
+            String txtSearch = request.getParameter("SearchBook");
+            HttpSession session = request.getSession();
+            BookDAO dao = new BookDAO();
+            List<BookDTO> list = dao.SearchBook(txtSearch);          
+            if (list.size() > 0) {
+                session.setAttribute("LIST_BOOK", list);//ATTRIBUTE CHU HOA 
+                url = SUCCESS;
+            }
+
+            
+        } catch (Exception e) {
+            log("Error at SearchBookController: " + e.toString());
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
