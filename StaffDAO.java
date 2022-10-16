42499cec07584ef69f10df9b3662b51eb59e774f
vungocthy <85751017+vungocthy@users.noreply.github.com>
2022-10-16 23:24:46 +0700

    createStaff and searchStaff for role admin
    Tham khảo để locate các file, nếu thấy không ổn thì có thể đổi thành locate khác:
    RegisterStaffController locate at controller.login,
    SearchStaffController locate at controller.search,
    createStaff.jsp locate at WEB-INF/JSP/ManagePage/createStaff.jsp,
    searchStaff.jsp locate at WEB-INF/JSP/ManagePage/searchStaff.jsp
    StaffDAO locate at dao/StaffDAO - tất cả function trong StaffDAO đều là function mới
    StaffErrorDTO locate at dto/StaffErrorDTO

--- StaffDAO.java
+++ StaffDAO.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
 
@@ -10,39 +9,62 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import utilities.DBUtils;
+import java.util.ArrayList;
+import java.util.List;
+import utils.DBUtils;
 
 /**
  *
- * @author Administrator
+ * @author vungo
  */
+//Code thêm vô dao/StaffDAO. Tất cả function dưới là mới hết
 public class StaffDAO {
-
-    private static final String LOGIN = "SELECT name, [role], [Date-of-birth], phone, [status], [delete] FROM tblStaff WHERE staffID LIKE ? AND [password] LIKE ?";
-    private static final String UPDATE_STATUS_ONLINE = "UPDATE tblStaff SET [status] = ? WHERE staffID LIKE ?";
-    private static final String CHECK_CUSTOMER_IDinSTAFF="SELECT staffID FROM tblStaff WHERE staffID=?";
-    
-
-    public StaffDTO checkLogin(String userID, String password) throws SQLException {
-        StaffDTO staff = null;
+    public static final String CREATE_STAFF=
+"INSERT INTO tblStaff(staffID,[Name],[Password],[Role],[Phone],[Date-of-birth],[Status],[Delete])\n" +
+"VALUES(?,?,?,?,?,?,0,0)";
+    public static final String CHECK_STAFF_ID="SELECT staffID FROM tblStaff WHERE staffID LIKE ?";
+    public static final String LOAD_ALL_STAFF=
+"SELECT staffID,[Name],[Password],[Role],[Phone],[Date-of-birth],[Status],[Delete] FROM tblStaff";
+    public static final String SEARCH_STAFF=
+"SELECT staffID,[Name],[Password],[Role],[Phone],[Date-of-birth],[Status],[Delete] FROM tblStaff"
++"WHERE staffID LIKE ? OR [Name] LIKE ? OR [Role] LIKE ? OR [Phone] LIKE ? OR [Date-of-birth] LIKE ?";
+ public boolean createStaff(StaffDTO staff) throws SQLException{
+        boolean check=false;
+        Connection conn=null;
+        PreparedStatement ptm=null;
+        try{
+            conn=DBUtils.getConnection();
+            if(conn!=null){
+                ptm=conn.prepareStatement(CREATE_STAFF);
+                ptm.setString(1, staff.getStaffID());
+                ptm.setString(2, staff.getName());
+                ptm.setString(3, staff.getPassword());
+                ptm.setString(4, staff.getRole());
+                ptm.setString(5, staff.getPhone());
+                ptm.setString(6, staff.getDateOfBirth());
+                check=ptm.executeUpdate()>0?true:false;
+            }
+        }catch(Exception e){
+            
+        }finally{
+            if(ptm!=null) ptm.close();
+            if(conn!=null) conn.close();
+        }
+        return check;
+    }
+public boolean checkStaffID(String staffID) throws SQLException {
+        boolean check = false;
         Connection conn = null;
         PreparedStatement ptm = null;
         ResultSet rs = null;
         try {
             conn = DBUtils.getConnection();
             if (conn != null) {
-                ptm = conn.prepareStatement(LOGIN);
-                ptm.setString(1, userID);
-                ptm.setString(2, password);
+                ptm = conn.prepareStatement(CHECK_STAFF_ID);
+                ptm.setString(1, staffID);
                 rs = ptm.executeQuery();
                 if (rs.next()) {
-                    String name = rs.getString("name");
-                    String role = rs.getString("role");
-                    String dob = rs.getString("Date-of-birth");
-                    String phone = rs.getString("phone");
-                    String status = rs.getString("status");
-                    String delete = rs.getString("delete");
-                    staff = new StaffDTO(userID, name, password, role, phone, dob, status, delete);
+                    check = true;
                 }
             }
         } catch (Exception e) {
@@ -58,25 +80,36 @@ public class StaffDAO {
                 conn.close();
             }
         }
-        return staff;
+        return check;
     }
-
-    public boolean updateStatusOnline(String staffID) throws SQLException {
+public List<StaffDTO> getListStaff() throws SQLException {
+        List<StaffDTO> list = new ArrayList<>();
         Connection conn = null;
         PreparedStatement ptm = null;
-        boolean check = false;
-        String status = "1";
+        ResultSet rs = null;
         try {
             conn = DBUtils.getConnection();
             if (conn != null) {
-                ptm = conn.prepareStatement(UPDATE_STATUS_ONLINE);
-                ptm.setString(1, status);
-                ptm.setString(2, staffID);
-                check = ptm.executeUpdate() > 0 ? true : false;
+                ptm = conn.prepareCall(LOAD_ALL_STAFF);
+                rs = ptm.executeQuery();
+                while (rs.next()) {
+                    String staffID = rs.getString("staffID");
+                    String name = rs.getString("Name");
+                    String password=rs.getString("Password");
+                    String role=rs.getString("Role");
+                    String phone=rs.getString("Phone");
+                    String dateOfBirth=rs.getString("Date-of-birth");
+                    String status=rs.getString("Status");
+                    String delete = rs.getString("Delete");
+                    list.add(new StaffDTO(staffID, name, password, role, phone, dateOfBirth, status, delete));
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            if (rs != null) {
+                rs.close();
+            }
             if (ptm != null) {
                 ptm.close();
             }
@@ -84,32 +117,46 @@ public class StaffDAO {
                 conn.close();
             }
         }
-        return check;
+        return list;
     }
-
-//check duplicate ID for customer in tblStaff
-    public boolean checkCusIDinStaff(String customerID) throws SQLException{
-        boolean check = false;
+public List<StaffDTO> searchStaff(String search) throws SQLException{
+        List<StaffDTO> listSearch = new ArrayList<>();
         Connection conn = null;
         PreparedStatement ptm = null;
         ResultSet rs = null;
-        try{
+        try {
             conn = DBUtils.getConnection();
-            if(conn!=null){
-                ptm = conn.prepareStatement(CHECK_CUSTOMER_IDinSTAFF);
-                ptm.setString(1, customerID);
+            if(conn != null){
+                ptm = conn.prepareStatement(SEARCH_STAFF);
+                ptm.setString(1, "%"+search+"%");
                 rs = ptm.executeQuery();
-                if(rs.next()){
-                    check=true;
+                while(rs.next()){
+                    String staffID = rs.getString("staffID");
+                    String name = rs.getString("name");
+                    String password = rs.getString("Password");
+                    String roleID = rs.getString("roleID");
+                    String phone = rs.getString("Phone");
+                    String dateOfBirth = rs.getString("Date-of-birth");
+                    String status=rs.getString("Status");
+                    String delete= rs.getString("delete");
+                    listSearch.add(new StaffDTO(staffID, name, password, roleID, phone, dateOfBirth, status, delete));
                 }
             }
-        }catch(Exception e){
+            
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-            if(rs!=null) rs.close();
-            if(ptm!=null) ptm.close();
-            if(conn!=null) conn.close();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         }
-        return check;
+        
+        return listSearch;
     }
 }
